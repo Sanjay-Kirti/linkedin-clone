640bc621dce2ae214ad9e803996a6dd6dbee4fd3
Sanjay Kirti <137834868+Sanjay-Kirti@users.noreply.github.com>
2025-08-03 20:15:15 +0530

    api.ts

--- client/app/utils/api.ts
+++ client/app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://linkedin-clone-backend-imc4.onrender.com",
 });
 
 export default api;
